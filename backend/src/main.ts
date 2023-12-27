f3289eea4d40eeaf80a2867ae3803e74c78a2a21
Dzianis Buyakou <denis.da.by@gmail.com>
2023-12-27 05:22:54 +0100

    feat: Add prisma module and update main app configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,32 @@
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const options = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  const websiteUrl = process.env.WEBSITE_URL;
 
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  app.enableCors({
+    origin: websiteUrl,
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle("Crypto Creatures API")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+  await app.listen(process.env.PORT || 3002);
 }
 bootstrap();
